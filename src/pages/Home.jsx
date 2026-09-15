a68df79208ee0ff035f29d2af848e6da2bed064c
Sneha Sutar <sutarsneha858@gmail.com>
2026-09-15 17:49:08 +0530

    update contact name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
-import Contact from '../components/contact'
+import Contact from '../components/Contact'
 
 
 
