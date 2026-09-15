a3cc3abee96ff5b5e039eec1503d7c4b03764ebf
Sneha Sutar <sutarsneha858@gmail.com>
2026-09-15 17:30:54 +0530

    update hero import path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/Hero'
+import hero from '../components/hero'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
@@ -10,7 +10,7 @@ import Contact from '../components/contact'
 const Home = () => {
   return (
     <div>
-      <Hero/>
+      <hero/>
       <About/>
       <Skills/>
       <Projects/>
